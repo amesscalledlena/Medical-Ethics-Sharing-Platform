b909f08fc26ba56afb359ec13ecf8dcbf4d96e97
Lena <itslena5831@gmail.com>
2026-05-13 12:06:43 +0300

    Finished the routings

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name = 'home'),
+    path('', views.home_view, name='home'),
+    path('index.html', views.home_view, name='return home'),
+    path('login.html', views.login_view, name='login'),
+    path('approved_posts.html', views.approved_posts_view, name='approved posts'),
+    path('create_post.html', views.create_post_view, name='create post'),
+    path('editor_panel.html', views.editor_panel_view, name='editor panel'),
+    path('post_detail.html', views.post_detail_view, name='post detail'),
+    path('register.html', views.register_view, name='register'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,20 @@ from django.shortcuts import render
 def home_view (request):
     return render (request, 'blog/index.html')
 
+def login_view (request):
+    return render (request, 'blog/login.html')
 
+def approved_posts_view (request):
+    return render (request, 'blog/approved_posts.html')
+
+def create_post_view (request):
+    return render(request, 'blog/create_post.html')
+
+def editor_panel_view (request):
+    return render(request, 'blog/editor_panel.html')
+
+def post_detail_view (request):
+    return render (request, 'blog/post_detail.html')
+
+def register_view (request):
+    return render (request, 'blog/register.html')
