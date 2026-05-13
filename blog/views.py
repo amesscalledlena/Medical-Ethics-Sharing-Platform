511fe19d5627b33cf54e63028a7a579b6d1bfca4
Lena <itslena5831@gmail.com>
2026-05-13 17:48:45 +0300

    Halfway through register logic

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def home_view (request):
     return render (request, 'blog/index.html')
 
 def login_view (request):
     error_message = None
-    
+
     if request.method == 'POST':
         u_name = request.POST.get('username')
         p_word = request.POST.get('password')
@@ -34,4 +35,19 @@ def post_detail_view (request):
     return render (request, 'blog/post_detail.html')
 
 def register_view (request):
-    return render (request, 'blog/register.html')
+    error_message = None
+
+    if request.method == 'POST':
+        u_name = request.POST.get('username')
+        email = request.POST.get('email', '')
+        p_word = request.POST.get('password')
+
+        if User.objects.filter(username=u_name).exists():
+            error_message = "This username is already taken. Please choose another."
+        else:
+            user = User.objects.create_user(username=u_name, email=email, password=p_word)
+
+            login(request, user)
+            return redirect('home')
+        
+    return render(request, 'blog/register.html', {'error': error_message})
